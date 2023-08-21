1ee82f6cc8d599a00b6f436b6bb61c65438dfeb5
Utkarsh singh tiwari <129392805+Utkarsh99999@users.noreply.github.com>
2023-08-21 17:42:44 +0530

    Update Form.jsx

--- Client/src/scenes/loginPage/Form.jsx
+++ Client/src/scenes/loginPage/Form.jsx
@@ -79,7 +79,7 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("http://localhost:6001/auth/login", {
+    const loggedInResponse = await fetch("https://faciogram-api-service.onrender.com/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
