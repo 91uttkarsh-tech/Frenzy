c2ddb7276e151324f6c6a8ec9b154fb3cbf1b88e
Utkarsh singh tiwari <129392805+Utkarsh99999@users.noreply.github.com>
2023-08-21 18:39:57 +0530

    Update Form.jsx

--- Client/src/scenes/loginPage/Form.jsx
+++ Client/src/scenes/loginPage/Form.jsx
@@ -64,7 +64,7 @@ const Form = () => {
     }
     formData.append("picturePath", values.picture.name);
     const savedUserResponse = await fetch(
-      "http://localhost:6001/auth/register",
+      "https://faciogram-api-service.onrender.com/auth/register",
       {
         method: "POST",
         body: formData,
